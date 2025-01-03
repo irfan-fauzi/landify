21d5528953d08685d5c9b4f07bc24100532b0c2f
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-01-03 22:50:29 +0700

    client-fetch

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import ListData from "@/components/ListData";
 export default async function Home() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   const dataPost = await res.json() 
+  const limitedData = dataPost.slice(0, 10);
   return (
    <div className="max-w-[600px] mx-auto mt-5">
     <h1 className="text-4xl">Home</h1>
@@ -10,12 +11,16 @@ export default async function Home() {
     <div className="border mt-3">
     {
       <ul className="list-decimal list-inside">
-        {dataPost.map((post) => (
+       {
+        limitedData.map((post) => (
           <li key={post.id}>{post.title}</li>
-        ))}
+        ))
+       }
       </ul>
     }
     </div>
+    <p className="mt-3">dibawah ini data di dapatkan dengan metode CSR use-clent</p>
+    <ListData />
    </div>
   );
 }
--- src/components/ListData.jsx
+++ src/components/ListData.jsx
@@ -1,12 +1,26 @@
 'use client'
-
 import { useEffect, useState } from "react"
 
 const ListData = () => {
-  
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const resJson = await res.json()
+      const limit10 = resJson.slice(0,10)
+      setData(limit10)
+    }
+    getPosts()
+    
+  },[])
   return (
     <>
-      <h1 className="text-red-500 text-3xl">List data</h1>
+      <h1 className="text-red-500 text-3xl">List data CSR Client :</h1>
+      <ul className="list-decimal list-inside">
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
